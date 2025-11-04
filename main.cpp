a7544a9bbc5b007ad786f8fa53bf3da4bb0015cf
VacationAir <vacationair.va@gmail.com>
2025-11-04 16:58:50 +0100

    Entry point of the program. Initializes an Aircraft object and runs the main simulation loop, updating its state over time.

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <airplane.cpp>
+
+int main(){
+    Aircraft cessna152(600, 0, 46.3, 82000, 14.9); // Parameters for a cesssna 152 at 90 Knots (46.3 m/s)
+    double dt = 0.001; // seconds
+    for(double i = 0; i < 60; i+= dt){ // 1 Minute -> 60 seconds
+        cessna152.update(dt);
+    }
+    return 0;
+}
