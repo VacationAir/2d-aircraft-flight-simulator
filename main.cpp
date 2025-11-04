fd0d9bf067cb58d2893366259c9117ed4e1bf748
VacationAir <vacationair.va@gmail.com>
2025-11-04 17:07:15 +0100

    Fixed wrong name when importing aircraft.cpp in main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <airplane.cpp>
+#include "aircraft.cpp"
 
 int main(){
     Aircraft cessna152(600, 0, 46.3, 82000, 14.9); // Parameters for a cesssna 152 at 90 Knots (46.3 m/s)
