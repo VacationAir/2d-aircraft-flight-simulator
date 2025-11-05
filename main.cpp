f5f6fe9aece4ac8cd2fbb6c2cfcd19c540f5663d
VacationAir <vacationair.va@gmail.com>
2025-11-05 20:46:17 +0100

    Update main.cpp to meet Aircraft class constructor requirements

--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 #include "aircraft.cpp"
 
 int main(){
-    Aircraft cessna152(600, 0, 46.3, 82000, 14.9); // Parameters for a cesssna 152 at 90 Knots (46.3 m/s)
-    double dt = 0.001; // seconds
+    Aircraft cessna152(600, 0, 46.3, 610, 0,  82000, 14.9); // Parameters for a cesssna 152 at 90 Knots (46.3 m/s) at 2000ft (610m) with a 4.1º angle
+    double dt = 0.01; // seconds
     for(double i = 0; i < 60; i+= dt){ // 1 Minute -> 60 seconds
         cessna152.update(dt);
+        std::cout << "Time: " << i << " seconds\n";
     }
     return 0;
 }
